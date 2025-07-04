7bd79e55dacb8c9602af407add8b64c74adedf9a
tomotele <tomotelechristopher25@gmail.com>
2025-07-04 12:01:40 +0100

    :Minor UI improvement

--- pages/404.js
+++ pages/404.js
@@ -1,15 +1,14 @@
 import Link from "next/link";
-
 export default function Custom404() {
   return (
     <div className="h-screen flex flex-col items-center justify-center bg-gray-50 text-center px-4">
       <h1 className="text-7xl font-extrabold text-black">404</h1>
       <p className="mt-4 text-xl text-gray-700">
-        Oops! The page you're looking for doesnt exist.
+        Oops! The page you&apos;re looking for doesn&apos;t exist.
       </p>
       <Link
         href="/"
-        className="mt-6 inline-block bg-white text-black px-6 py-2 rounded-md shadow hover:bg-blue-700 transition"
+        className="mt-6 inline-block bg-white text-black px-6 py-2 rounded-md shadow hover:bg-blue-700 hover:text-white transition"
       >
         Go back home
       </Link>
--- pages/500.js
+++ pages/500.js
@@ -1,4 +1,3 @@
-// pages/500.js
 import Link from "next/link";
 
 export default function Custom500() {
@@ -6,11 +5,11 @@ export default function Custom500() {
     <div className="h-screen flex flex-col items-center justify-center bg-orange-50 text-center px-4">
       <h1 className="text-7xl font-extrabold text-black">500</h1>
       <p className="mt-4 text-xl text-gray-800">
-        Something went wrong on our end. We're working on it.
+        Something went wrong on our end. We&apos;re working on it.
       </p>
       <Link
         href="/"
-        className="mt-6 inline-block bg-white text-black px-6 py-2 rounded-md shadow hover:bg-orange-600 transition"
+        className="mt-6 inline-block bg-white text-black px-6 py-2 rounded-md shadow hover:bg-orange-600 hover:text-white transition"
       >
         Try again later or return home
       </Link>
