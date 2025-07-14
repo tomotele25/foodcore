7994a73f79ae4183b59852bcd84156571c91b876
tomotele <tomotelechristopher25@gmail.com>
2025-07-14 10:23:03 +0100

    :Test

--- pages/checkout/[slug].js
+++ pages/checkout/[slug].js
@@ -109,7 +109,6 @@ const Checkout = () => {
     try {
       const res = await axios.post(`${BACKENDURL}/api/init-payment`, {
         amount: finalTotal,
-        totalAmount: finalTotal,
         email: `guest${Date.now()}@chowspace.com`,
         vendorId: vendor._id,
         tx_ref: txRef,
